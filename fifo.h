a82070f7e895528f7c061e8b2c3bc72c1e3e5ee7
folkert van heusden <mail@vanheusden.com>
2023-01-16 15:15:28 +0100

    rtl_sdr_tcp class

---
+++ fifo.h
@@ -0,0 +1,197 @@
+// (C) 2022-2023 by folkert van heusden <mail@vanheusden.com>, released under Apache License v2.0
+#pragma once
+
+#include <optional>
+#include <pthread.h>
+#include <stdint.h>
+
+
+template <typename T>
+class fifo
+{
+private:
+	T        *data       { nullptr };
+	const int n_elements { 0       };
+
+	int  read_pointer    { 0       };
+	int  write_pointer   { 0       };
+	bool full            { false   };
+
+	bool interrupted     { false   };
+
+	pthread_mutex_t lock;
+
+	/* cond_push is signalled when a new item is pushed
+	 * cond_pull is signalled when an item is removed
+	 */
+	pthread_cond_t cond_push;
+	pthread_cond_t cond_pull;
+
+	uint64_t    n_in { 0       };
+
+	uint64_t   *fifo_n_in_stats { nullptr };
+
+public:
+	fifo(const int n_elements) : n_elements(n_elements)
+	{
+		data = new T[n_elements];
+
+		pthread_mutex_init(&lock, NULL);
+
+		pthread_cond_init(&cond_push, NULL);
+		pthread_cond_init(&cond_pull, NULL);
+	}
+
+	~fifo()
+	{
+		delete [] data;
+	}
+
+	int get_n_in()
+	{
+		pthread_mutex_lock(&lock);
+		int rc = n_in;
+		pthread_mutex_unlock(&lock);
+
+		return rc;
+	}
+
+	void interrupt()
+	{
+		pthread_mutex_lock(&lock);
+
+		interrupted = true;
+
+		pthread_cond_broadcast(&cond_push);
+
+		pthread_mutex_unlock(&lock);
+	}
+
+	void put(const T & element)
+	{
+		pthread_mutex_lock(&lock);
+
+		while(full)
+			pthread_cond_wait(&cond_pull, &lock);
+
+		data[write_pointer] = *element;
+
+		write_pointer++;
+		write_pointer %= n_elements;
+
+		n_in++;
+
+		full = write_pointer == read_pointer;
+
+		pthread_cond_signal(&cond_push);
+
+		pthread_mutex_unlock(&lock);
+	}
+
+	bool try_put(const T & element)
+	{
+		bool have_put = false;
+
+		pthread_mutex_lock(&lock);
+
+		if (!full) {
+			data[write_pointer] = element;
+
+			write_pointer++;
+			write_pointer %= n_elements;
+
+			n_in++;
+
+			full = write_pointer == read_pointer;
+
+			have_put = true;
+
+			pthread_cond_signal(&cond_push);
+		}
+
+		pthread_mutex_unlock(&lock);
+
+		return have_put;
+	}
+
+	std::optional<T> get(const int ms)
+	{
+		struct timespec ts { 0, 0 };
+		clock_gettime(CLOCK_REALTIME, &ts);
+
+		ts.tv_nsec += (ms % 1000) * 1000000ll;
+		ts.tv_sec += ms / 1000;
+
+		if (ts.tv_nsec >= 1000000000ll) {
+			ts.tv_nsec -= 1000000000ll;
+			ts.tv_sec++;
+		}
+
+		pthread_mutex_lock(&lock);
+
+		bool ok = true;
+
+		while(read_pointer == write_pointer && !full && !interrupted) {
+			if (pthread_cond_timedwait(&cond_push, &lock, &ts)) {
+				ok = false;
+				break;
+			}
+		}
+
+		if (interrupted) {
+			pthread_mutex_unlock(&lock);
+
+			return { };
+		}
+
+		if (ok) {
+			T copy = data[read_pointer];
+
+			read_pointer++;
+			read_pointer %= n_elements;
+
+			n_in--;
+
+			full = 0;
+
+			pthread_cond_signal(&cond_pull);
+
+			pthread_mutex_unlock(&lock);
+
+			return copy;
+		}
+
+		pthread_mutex_unlock(&lock);
+
+		return { };
+	}
+
+	std::optional<T> get()
+	{
+		pthread_mutex_lock(&lock);
+
+		while(read_pointer == write_pointer && !full && !interrupted)
+			pthread_cond_wait(&cond_push, &lock);
+
+		if (interrupted) {
+			pthread_mutex_unlock(&lock);
+
+			return { };
+		}
+
+		T copy = data[read_pointer];
+
+		read_pointer++;
+		read_pointer %= n_elements;
+
+		n_in--;
+
+		full = 0;
+
+		pthread_cond_signal(&cond_pull);
+
+		pthread_mutex_unlock(&lock);
+
+		return copy;
+	}
+};
--- main.cpp
+++ main.cpp
@@ -1,13 +1,117 @@
+#include <atomic>
 #include <math.h>
 #include <samplerate.h>
 #include <sndfile.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string>
+#include <thread>
 #include <unistd.h>
 
-#include "filter.h"
+#include "fifo.h"
 #include "net.h"
 
+
+class rtl_sdr_tcp
+{
+private:
+	struct iq_data {
+		uint8_t data[4096];
+	};
+
+	fifo<iq_data>    *iqs       { nullptr };
+
+	const std::string host;
+	const int         port      { 1234    };
+
+	std::thread      *th        { nullptr };
+	std::atomic_bool  stop_flag { false   };
+
+	int               fd        { -1      };
+
+	std::atomic_uint32_t frequency { 96800000 };
+
+	const int         sample_rate  { 1800000  };
+
+	void set_samplerate(const uint32_t sr) {
+		uint8_t msg[] = { 0x02, uint8_t(sr >> 24), uint8_t(sr >> 16), uint8_t(sr >> 8), uint8_t(sr) };
+
+		if (WRITE(fd, reinterpret_cast<const char *>(msg), sizeof msg) == -1) {
+			close(fd);
+
+			fd = -1;
+		}
+	}
+
+public:
+	rtl_sdr_tcp(const std::string & host, const int port) : host(host), port(port) {
+		iqs = new fifo<iq_data>(256);
+
+		th  = new std::thread(std::ref(*this));
+	}
+
+	virtual ~rtl_sdr_tcp() {
+		stop_flag = true;
+
+		th->join();
+		delete th;
+
+		delete iqs;
+	}
+
+	auto get_iqs() { return iqs; }
+
+	void set_frequency(const uint32_t f) {
+		frequency = f;
+
+		// TODO locking in case we were reconnecting concurrently
+
+		uint8_t msg[] = { 0x01, uint8_t(f >> 24), uint8_t(f >> 16), uint8_t(f >> 8), uint8_t(f) };
+
+		if (WRITE(fd, reinterpret_cast<const char *>(msg), sizeof msg) == -1) {
+			close(fd);
+
+			fd = -1;
+		}
+	}
+
+	void operator()() {
+		iq_data id;
+
+		while(!stop_flag) {
+			if (fd == -1) {
+				fd = connect_to(host.c_str(), port);
+
+				uint8_t dummy[12];
+				if (READ(fd, dummy, 12) != 12) {  // header
+					close(fd);
+
+					fd = -1;
+				}
+			}
+
+			if (fd != -1)
+				set_frequency(frequency);
+
+			if (fd != -1)
+				set_samplerate(sample_rate);
+
+			if (fd == -1)
+				continue;
+
+			if (READ(fd, id.data, sizeof id.data) != sizeof id.data) {
+				close(fd);
+
+				fd = -1;
+			}
+
+			if (!iqs->try_put(id))
+				printf("buffer full\n");
+		}
+
+		close(fd);
+	}
+};
 
 void resample(const float *const in_float, const int in_rate, const int n_samples, float **const out, const int out_rate, int *const out_n_samples)
 {
@@ -29,107 +133,48 @@ void resample(const float *const in_float, const int in_rate, const int n_sample
         int rc = -1;
         if ((rc = src_simple(&sd, SRC_SINC_BEST_QUALITY, 1)) != 0)
                 printf("SIP: resample failed: %s\n", src_strerror(rc));
-
-	printf("%ld %ld %d\n", sd.input_frames_used, sd.output_frames_gen, sd.end_of_input);
-}
-
-void set_frequency(const int fd, const uint32_t f)
-{
-	uint8_t msg[] = { 0x01, f >> 24, f >> 16, f >> 8, f };
-
-	WRITE(fd, reinterpret_cast<const char *>(msg), sizeof msg);
-}
-
-void set_samplerate(const int fd, const uint32_t sr)
-{
-	uint8_t msg[] = { 0x02, sr >> 24, sr >> 16, sr >> 8, sr };
-
-	WRITE(fd, reinterpret_cast<const char *>(msg), sizeof msg);
 }
 
 int main(int argc, char *argv[])
 {
-	printf("init\n");
+	// rtl_sdr_tcp sdr_instance("10.208.30.7", 12345);
+	rtl_sdr_tcp sdr_instance("192.168.64.226", 12444);
 
-	// host, ip of the rtl_tcp server
-	int fd = connect_to("192.168.64.226", 12444);
-	//int fd = connect_to("10.208.30.7", 12345);
-	if (fd == -1) {
-		fprintf(stderr, "Cannot connect to host\n");
-		return 1;
+	sdr_instance.set_frequency(96000000);
+
+	auto iqs = sdr_instance.get_iqs();
+
+	for(;;) {
+		auto   iq  = iqs->get();
+
+		if (iq.has_value() == false)
+			break;
+
+		int    n_in = iqs->get_n_in();
+		if (n_in)
+			printf("%d\n", n_in);
+
+		auto   values = iq.value().data;
+
+		int    n_values = sizeof(values) / 2;
+
+		float  decoded[n_values];
+
+		double in1    = 0;
+		double qn1    = 0;
+
+		for(int i=0; i<n_values; i++) {
+			int o = i * 2;
+
+			double in = (values[o + 0] - 128) / 128.0;
+			double qn = (values[o + 1] - 128) / 128.0;
+
+			decoded[i] = (in1 * qn - in * qn1) / (in * in + qn * qn);
+
+			in1 = in;
+			qn1 = qn;
+		}
 	}
-
-	uint8_t dummy[12];
-	READ(fd, dummy, 12);  // header
-
-	//uint64_t f = 172274300;
-	uint64_t f = 96000000;
-	uint64_t f_target = 12000;
-
-	set_frequency(fd, f);  // tune
-
-	constexpr int samplerate = 1800000;  // 1.8MHz
-	constexpr int duration = 10;  // 60s
-
-	set_samplerate(fd, samplerate);
-
-	uint8_t *iq = new uint8_t[samplerate * 2 * duration];
-
-	printf("starting sampling\n");
-
-	printf("read: %zd\n", READ(fd, iq, samplerate * 2 * duration));
-
-	printf("sampled\n");
-
-	float *buffer = new float[samplerate * duration]();
-
-	constexpr float gain = 1.f;
-
-	FilterButterworth hp(100.0, samplerate, true, sqrt(2.0));
-	FilterButterworth lp(48000.0, samplerate, false, sqrt(2.0));
-
-	for(int k=1; k<samplerate * duration; k++) {
-		double y_t = sin(2. * M_PI * f_target * k / samplerate);
-
-		int o = k * 2;
-
-		double in = (iq[o + 0] - 128) / 128.0;
-		double qn = (iq[o + 1] - 128) / 128.0;
-
-		double in1 = (iq[o + 0 - 2] - 128) / 128.0;
-		double qn1 = (iq[o + 1 - 2] - 128) / 128.0;
-
-		buffer[k] = (in1 * qn - in * qn1) / (pow(in, 2.) + pow(qn, 2.));
-
-		//buffer[k] = lp.apply(hp.apply(shifted));
-		//buffer[k] = shifted;
-	}
-
-	printf("converted\n");
-
-	close(fd);
-
-	constexpr int samplerate_audio = 48000;
-
-	SF_INFO si;
-	si.frames = 0;
-	si.samplerate = samplerate_audio;
-	si.channels = 1;
-	si.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
-	si.sections = 0;
-	si.seekable = 0;
-
-	SNDFILE *sf = sf_open("test.wav", SFM_WRITE, &si);
-
-	int out_n = 0;
-	float *out = nullptr;
-	resample(buffer, samplerate, samplerate * duration, &out, samplerate_audio, &out_n);
-
-	printf("resampled %d -> %d\n", samplerate * duration, out_n);
-
-	sf_writef_float(sf, out, out_n);
-
-	sf_close(sf);
 
 	return 0;
 }
